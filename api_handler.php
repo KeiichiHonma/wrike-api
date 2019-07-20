a6dc34df57347c03a790e9e2b26ae916e4cfcaa5
keiichi-honma <keiichi1125@gmail.com>
2019-07-20 22:50:59 +0900

    アップ成功

---
+++ api_handler.php
@@ -0,0 +1,226 @@
+<?php
+// Check this constant. Just to see that we are not being directly requested...
+if(!defined('angus')) {
+    die('Brothers Malcolm, Angus, and George Young were born in Glasgow, Scotland, and moved to Sydney with most of their family in 1963.');
+}
+
+class Wrikeapi
+{
+    // Set the the auth credentials obtained from wrike
+    private $client_id = '<client id>';
+    private $client_secret = '<shhh... its a secret!>';
+    private $code = '<code>'; // when you agree to give acces through the api, you get this code.
+    private $refresh_token = '<token for refreshing .. the token>';
+    private $api_url = 'https://www.wrike.com/api/v4'; // the url of the api
+    // recieve the token
+    
+    /*
+     * Run this method to get the refresh_token. It should only be run once as it will reset the authentification and requre a fresh auth-code.
+     */
+    function token_init()
+    {
+        $url = "https://www.wrike.com/oauth2/token";
+        $fields = array(
+            'client_id'=>$this->client_id, 
+            'client_secret'=>$this->client_secret, 
+            'grant_type'=>'authorization_code',
+            'code'=>$this->code
+            );
+        $token = $this->curl_post($url, $fields);
+        $token = json_decode($token);
+        echo "note: This should only be run ONCE for your application. From here on, only REFRESH - because asking for a new one will require you to update the authorization_code";
+        return $token; // be sure to keep the refresh token handy (comes with the json).
+    }
+
+    // call this to refresh
+    private function _refresh_token()
+    {
+        $url = "https://www.wrike.com/oauth2/token";
+        $fields = array(
+            'client_id'=>$this->client_id, 
+            'client_secret'=>$this->client_secret, 
+            'grant_type'=>'refresh_token', 
+            'refresh_token'=>$this->refresh_token
+            );
+        $token = curl_post($url, $fields);
+        return $token;
+    }
+    
+    function api_version($token)
+    {
+        $url = 'https://www.wrike.com/api/v4/version';
+        $result = $this->_fetch_result($token,$url);
+        return $result;
+    }
+    
+    /*
+     * Information about the accounts this user can access
+     * Plese note the ID returned
+     */
+    function get_accounts($token)
+    {
+        $url = $this->api_url . '/accounts';
+        $result = $this->curl_get($url, $token);
+        return $result;
+    }
+    function get_folders($token, $accountid = '2946201') 
+    {
+        if($accountid == ''){
+            echo "If you wont provide me with the needed input, i wont provide you with the output...";exit;
+        }
+        //$url = $this->api_url . '/accounts/' . $accountid . '/folders';
+        $url = $this->api_url . '/folders';
+        $result = $this->_fetch_result($token,$url);
+        return $result;
+    }
+    function get_tasks($token, $folder, $params = '')
+    {
+        $url = $this->api_url . '/folders/'.$folder.'/tasks';
+        $result = $this->_fetch_result($token,$url);
+        return $result;
+    }
+    function create_tasks($token, $folder, $params = '')
+    {
+        $fields = array(
+            'title'=>'task title 2',
+            );
+        $url = $this->api_url . '/folders/'.$folder.'/tasks';
+        $result = $this->_fetch_result_post($token,$url,$fields);
+        return $result;
+    }
+    function attachments_folders($token, $folder, $params = '')
+    {
+      //$result = $this->curl_get_file('https://attachments.clickup.com/45e9ea56-940e-45d3-a794-d674472d2c2b/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202019-03-15%2012.55.34.png');
+        $url = $this->api_url . '/folders/'.$folder.'/attachments';
+        $result = $this->curl_post_file($url, $token,'https://attachments.clickup.com/45e9ea56-940e-45d3-a794-d674472d2c2b/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202019-03-15%2012.55.34.png');
+        return $result;
+    }
+    /*
+     * Get the job done - or provide you with a reaon why i was not done
+     */
+    private function _fetch_result($token = '',$url = '') 
+    {
+        if($token == '' || $url == '') {
+            echo "FUCK ME! - Something was not right - take a look at this:";
+            echo "... you are not giving me enough to work with. I am missing params!";
+            exit('SUICIDE');
+        }
+        
+        $result = $this->curl_get($url, $token);
+        $evaluate_result = json_decode($result);
+        if(isset($evaluate_result->error)){
+            echo "FUCK ME! - Something was not right - take a look at this:";
+            var_dump($evaluate_result);exit('SUICIDE');
+        } else { 
+            return $result;
+        }
+    }
+
+    private function _fetch_result_post($token = '',$url = '',$fields = '') 
+    {
+        if($token == '' || $url == '' || $fields == '') {
+            echo "FUCK ME! - Something was not right - take a look at this:";
+            echo "... you are not giving me enough to work with. I am missing params!";
+            exit('SUICIDE');
+        }
+        
+        $result = $this->curl_wrike_post($url, $token, $fields);
+        $evaluate_result = json_decode($result);
+        if(isset($evaluate_result->error)){
+            echo "FUCK ME! - Something was not right - take a look at this:";
+            var_dump($evaluate_result);exit('SUICIDE');
+        } else { 
+            return $result;
+        }
+    }
+
+    function curl_post($url,$fields)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        $result = curl_exec($ch);
+        return $result;
+    }
+    
+    function curl_get($url,$token)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $token ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        $result = curl_exec($ch);
+        return $result;
+    }
+    
+    function curl_wrike_post($url,$token,$fields)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $token ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        $result = curl_exec($ch);
+        return $result;
+    }
+
+    function curl_get_file($url)
+    {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_POST, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+      $result = curl_exec($ch);
+      return $result;
+    }
+
+    function curl_post_file($url,$token,$file_url)
+    {
+        $obj = file_get_contents($file_url);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $header = [
+            'Authorization: bearer ' . $token,
+            'Content-Type: application/octet-stream',
+            'X-Requested-With: XMLHttpRequest',
+            'X-File-Name: attachment.png'
+        ];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $obj);
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $result);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE); // --data-binary
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        
+        $result = curl_exec($ch);
+        $info   = curl_getinfo($ch);
+        if($result === false)
+        {
+            echo 'Curl error: ' . curl_error($ch);
+        }
+        else
+        {
+            echo 'Operation completed without any errors';
+        }
+        return $result;
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    define('angus',TRUE); // setting a constant in order to prevent direct access. Low practical - i know...
+    require 'api_handler.php';
+    $api = new wrikeapi();
+    // Look to the api_handler.php - method token_init can help you make the access token.
+    // you should be able to recieve the token.
+    $access_token = 'eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjI5NDYyMDEsXCJpXCI6NjM4MDk0OCxcImNcIjo0NjEyNTE1LFwidVwiOjYyOTYzNTYsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkFcIixcIkxcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE1NjM0NDEzMjZ9.rimp4lgSo9HAr36wlW0NN99hHBZlD4IusTaXHI-0EcI';
+?>
+<!DOCTYPE html>
+<!--
+ Morten D. Hansen
+ 2015 morten@visia.dk
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Wrike API</title>
+    </head>
+    <body>
+        <p>running</p>
+        <?php
+            // check that the API is running - this gets the version
+            //$version = $api->get_folders($access_token);
+            //$version = $api->create_tasks($access_token, 'IEACZ5EZI4LGZILT');
+            $version = $api->attachments_folders($access_token, 'IEACZ5EZI4LGZILT');
+var_dump($version);
+die();
+            $version = json_decode($version);
+var_dump($version);
+die();
+            echo "<p>The api version you are requesting is: " . $version->data[0]->major . "." . $version->data[0]->minor . "<p>";
+            echo "That went well!";
+        ?>
+    </body>
+</html>
